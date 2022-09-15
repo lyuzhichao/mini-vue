fcdaf335feeabcf6585a3898242f184a73e855b7
LZV2SZH <zhichao.lv@cn.bosch.com>
2022-09-15 23:20:39 +0800

    Update codes for mountElement

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,14 @@ import h from '../../lib/guide-mini-vue.esm'
 export const App={
     //render is required
    render(){
-       return h('div','hi,'+this.msg)
+       return h('div',
+           {
+           id:'root',
+           class:['red','hard']
+       },
+           // 'hi,mini-vue'
+           [h('p',{class:'red'},'hi'),h('p',{class:'blue'},'mini-vue')]
+       )
    },
     setup(){
        //composition api
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,6 @@
 import {createComponentInstance,setUpComponent} from "./component";
+// @ts-ignore
+import {isObject} from "../shared";
 
 export function render(vnode,container){
     //call patch function
@@ -8,8 +10,37 @@ export function render(vnode,container){
 export function patch(vnode,container){
     //process component
     //if vnode is element, call processElement
-    // processElement()
-    processComponent(vnode,container)
+    if (typeof vnode.type==="string"){
+        processElement(vnode,container)
+    } else if (isObject(vnode.type)){
+        processComponent(vnode,container)
+    }
+}
+export function processElement(vnode,container){
+    mountElement(vnode,container)
+}
+
+export function mountElement(vnode,container){
+    //vnode.children -> string / array
+    const el=document.createElement(vnode.type)
+    const {children}=vnode
+    if (typeof children==="string"){
+        el.textContent=children
+    } else if (Array.isArray(children)){
+        mountChildren(vnode,el)
+    }
+    const {props}=vnode
+    for (const key in props){
+        const val=props[key]
+        el.setAttribute(key,val)
+    }
+    container.append(el)
+}
+
+export function mountChildren(vnode,container){
+    vnode.children.forEach(child=>{
+        patch(child,container)
+    })
 }
 
 export function processComponent(vnode,container){
